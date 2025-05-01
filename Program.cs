24e5fc3bd86da7cfdcfa023092b7b662678e1419
BadrBoukries <148701413+Badr8Boukries@users.noreply.github.com>
2025-05-01 17:35:30 +0200

    update : ajout de configuration de stmp et jwt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-using Microsoft.EntityFrameworkCore; 
-using CastFlow.Api.Data;          
+using Microsoft.EntityFrameworkCore;
+using CastFlow.Api.Data;
+using CastFlow.Api.Repository;          
+using CastFlow.Api.Services;             
+using CastFlow.Api.Services.Interfaces; 
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,13 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrEmpty(connectionString))
 {
-  
     Console.WriteLine("Erreur: Chaîne de connexion 'DefaultConnection' non trouvée dans appsettings.json");
 }
 
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString)); // Utilisation de UseSqlServer
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 
@@ -23,6 +24,10 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "CastFlow API", Version = "v1" });
 });
 
+builder.Services.AddScoped<IUserTalentRepository, UserTalentRepository>();
+builder.Services.AddScoped<IUserAdminRepository, UserAdminRepository>();
+
+builder.Services.AddScoped<ITalentService, TalentService>(); 
 
 
 var app = builder.Build();
@@ -30,24 +35,24 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(); 
+    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "CastFlow API v1");
-        c.RoutePrefix = string.Empty; 
+        c.RoutePrefix = string.Empty;
     });
     app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseExceptionHandler("/Error"); 
-    app.UseHsts(); 
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();  
-app.MapControllers();
 
+app.MapControllers();
 
 app.Run();
